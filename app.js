454ab608c1606f80ad5f75d036389d6316ec3a4d
Mahmoud Sayed Hassan <mahmoudsayed671@gmail.com>
2024-06-28 10:15:56 +0300

    serving static files

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express();
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+
 //Middlewares
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`)); //now we can get the static files like html-images... from the browser ex:(127.0.0.1:3000/overview.html)
 app.use((req, res, next) => {
   console.log('Hello from the middleware 👋');
   next();
