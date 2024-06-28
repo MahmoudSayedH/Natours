aff6f026b0fd25c440d53f5d3d54ed7a68fa6cd5
Mahmoud Sayed Hassan <mahmoudsayed671@gmail.com>
2024-06-28 12:02:34 +0300

    create mongoose schema & model

--- server.js
+++ server.js
@@ -10,6 +10,32 @@ const db =
   process.env.DATABASE_LOCAL;
 mongoose.connect(db).then(() => console.log('connected to db'));
 
+//create schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+//create model
+const Tour = mongoose.model('Tour', tourSchema);
+
+//test the model
+const testTour = new Tour({ name: 'test tour', price: 500 });
+testTour
+  .save()
+  .then(doc => console.log(doc))
+  .catch(err => console.log(err));
 //Start Server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
