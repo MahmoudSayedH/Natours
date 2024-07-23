0ed849e9c46db0ca887e00efc5625c33c5969a48
Mahmoud Sayed Hassan <mahmoudsayed671@gmail.com>
2024-07-23 19:37:33 +0300

    add cast error handler

--- middlewares/globalErrorMiddleware.js
+++ middlewares/globalErrorMiddleware.js
@@ -1,13 +1,6 @@
 const AppError = require('./../util/AppError');
 
 const handleCasteErrorDB = err => new AppError(`Invalid ${err.path}: ${err.value}`, 404);
-const handleDuplicateFieldsDB = err => new AppError(`The id: "${err.keyValue.name}" is already taken`, 404);
-const handleValidationErrorDB = err => {
-  const message = Object.values(err.errors).map(el => {
-    return { [el.path]: el.message };
-  });
-  return new AppError(JSON.stringify(message), 404);
-};
 
 const sendErrorDev = (err, res) => {
   console.error(err);
@@ -41,8 +34,6 @@ module.exports = (err, req, res, next) => {
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err, name: err.name };
     if (error.name === 'CastError') error = handleCasteErrorDB(error);
-    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
 
     sendErrorProd(error, res);
   }
